20079a7e42ab44eeae9fa5ab74d382386072095d
zhangpanweb <zhangpanweb@163.com>
2019-04-09 11:36:20 +0800

    finish calendar

--- components/calendar/index.jsx
+++ components/calendar/index.jsx
@@ -1,69 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import propTypes from 'prop-types';
 import moment from 'moment';
 
 import './style.less';
-
-// function getDaysOfMonth (year, month) {
-//   const intYear = parseInt(year, 10);
-//   const intMonth = parseInt(month, 10);
-//   if ([1, 3, 5, 7, 8, 10, 12].indexOf(intMonth) !== -1) {
-//     return 31;
-//   } else if (intMonth === 2) {
-//     if (intYear % 4 === 0) return 28;
-//     return 29;
-//   } else {
-//     return 30;
-//   }
-// }
-
-const Calendar = ({ value = new Date() }) => {
-  value = '2016-03-20';
-  const firstDayOfMonth = moment(value).startOf('month').format('YYYY-MM-DD');
-  const lastDayOfMonth = moment(value).endOf('month').format('DD');
-  const lastDateOfMonth = moment(value).endOf('month').format('YYYY-MM-DD');
-  let lastDayOfLastMonth = moment(value).add(-1, 'months').endOf('month').format('DD');
-  const firstDay = covertDateToDay(firstDayOfMonth);
-  const lastDay = covertDateToDay(lastDateOfMonth);
-
-  let dates = [];
-  let index = 1;
-
-  for (let i = 1; i <= lastDayOfMonth; i++) {
-    dates.push(i);
-  }
-  for (let i = firstDay; i > 1; i--) {
-    dates.unshift(lastDayOfLastMonth--);
-  }
-  for (let i = lastDay; i < 7; i++) {
-    dates.push(index++);
-  }
-
-  let key = 1;
-  return (
-    <div className="calendar-container">
-      <div className="title">
-        <span>{'<<'}</span>
-        <span>{'<'}</span>
-        <span>日期</span>
-        <span>{'>'}</span>
-        <span>{'>>'}</span>
-      </div>
-
-      <div className="date">
-        {dates.map(date => {
-          return (
-            <span className="cell" key={key++}>{date}</span>
-          );
-        })}
-      </div>
-    </div>
-  );
-};
-
-Calendar.propTypes = {
-  value: propTypes.object
-};
 
 const refrenceDate = {
   date: '1970-01-05',
@@ -80,5 +19,106 @@ function covertDateToDay (date) {
 
   return refrenceDate.day + index;
 }
+
+// function getDaysOfMonth (year, month) {
+//   const intYear = parseInt(year, 10);
+//   const intMonth = parseInt(month, 10);
+//   if ([1, 3, 5, 7, 8, 10, 12].indexOf(intMonth) !== -1) {
+//     return 31;
+//   } else if (intMonth === 2) {
+//     if (intYear % 4 === 0) return 28;
+//     return 29;
+//   } else {
+//     return 30;
+//   }
+// }
+
+const Calendar = ({ value = new Date() }) => {
+  const [selectedDate, setSelectedDate] = useState(parseInt(moment(value).format('DD'), 10));
+  const [selectedMonth, setSelectedMonth] = useState(moment(value).format('YYYY-MM-DD'));
+  const [monthDates, setMonthDates] = useState([]);
+
+  useEffect(() => {
+    formatDates();
+  }, [selectedMonth]);
+
+  const formatDates = () => {
+    const dates = [];
+    const firstDateOfMonth = moment(selectedMonth).startOf('month').format('YYYY-MM-DD');
+    const firstDay = covertDateToDay(firstDateOfMonth);
+
+    const lastDateNumberOfMonth = moment(selectedMonth).endOf('month').format('DD');
+    const lastDateOfMonth = moment(selectedMonth).endOf('month').format('YYYY-MM-DD');
+    const lastDay = covertDateToDay(lastDateOfMonth);
+
+    /** [1,2,3,4,...,30] */
+    for (let i = 1; i <= lastDateNumberOfMonth; i++) {
+      dates.push(i);
+    }
+
+    /** 补充前一个月和后一个月的空白部分 */
+    for (let i = firstDay; i > 1; i--) {
+      dates.unshift(undefined);
+    }
+    for (let i = lastDay; i < 7; i++) {
+      dates.push(undefined);
+    }
+
+    setMonthDates(dates);
+  };
+
+  /** 处理各类交互 */
+  const handleSelectPreMonth = () => {
+    const lastMonth = moment(selectedMonth).add(-1, 'months');
+    setSelectedMonth(lastMonth);
+  };
+
+  const handleSelectNextMonth = () => {
+    const nextMonth = moment(selectedMonth).add(1, 'months');
+    setSelectedMonth(nextMonth);
+  };
+
+  const handleSelectedDate = (date) => {
+    setSelectedDate(date);
+  };
+
+  let key = 1;
+  return (
+    <div className="calendar-container">
+      <div className="title">
+        <img src="/static/images/calendar-left.png" onClick={handleSelectPreMonth}/>
+        <span>日期</span>
+        <img src="/static/images/calendar-right.png" onClick={handleSelectNextMonth}/>
+      </div>
+
+      <div className="day">
+        <span>S</span>
+        <span>M</span>
+        <span>T</span>
+        <span>W</span>
+        <span>T</span>
+        <span>F</span>
+        <span>S</span>
+      </div>
+
+      <div className="date">
+        {monthDates.map(date => {
+          let className = 'cell';
+          if (date === selectedDate) {
+            className = 'cell selected';
+          }
+          return (
+            <span className={className} key={key++} onClick={() => { handleSelectedDate(date); }}>{date}</span>
+          );
+        })}
+      </div>
+
+    </div>
+  );
+};
+
+Calendar.propTypes = {
+  value: propTypes.object
+};
 
 export default Calendar;
