d432791b154ba66c0decd46e2e6a523290a6b34b
zhangpanweb <zhangpanweb@163.com>
2019-05-07 13:14:22 +0800

    calendar component: adjust some code

--- web/components/calendar/index.jsx
+++ web/components/calendar/index.jsx
@@ -25,7 +25,7 @@ const Calendar = ({ value = new Date(), onChangeDate }) => {
   const nowDate = parseInt(moment().format('DD'), 10);
 
   const [selectedDate, setSelectedDate] = useState(parseInt(moment(value).format('DD'), 10)); // 被选中的这个月的第几天，类型为数字
-  const [selectedMonth, setSelectedMonth] = useState(moment(value).format('YYYY-MM-DD')); // 被选中的月份，格式为 YYYY-MM-DD
+  const [selectedMonth, setSelectedMonth] = useState(moment(value).format('YYYY-MM')); // 被选中的月份，格式为 YYYY-MM
   const [monthDates, setMonthDates] = useState([]); // 日历上需要展示的这个月的天数组成的数组
 
   useEffect(() => {
@@ -34,7 +34,7 @@ const Calendar = ({ value = new Date(), onChangeDate }) => {
 
   /** 处理各类交互 */
   const handleSelectPreMonth = () => {
-    const lastMonth = moment(selectedMonth).add(-1, 'months').format('YYYY-MM-DD');
+    const lastMonth = moment(selectedMonth).add(-1, 'months').format('YYYY-MM');
     setSelectedMonth(lastMonth);
   };
 
@@ -43,7 +43,7 @@ const Calendar = ({ value = new Date(), onChangeDate }) => {
       return;
     }
 
-    const nextMonth = moment(selectedMonth).add(1, 'months').format('YYYY-MM-DD');
+    const nextMonth = moment(selectedMonth).add(1, 'months').format('YYYY-MM');
     setSelectedMonth(nextMonth);
   };
 
@@ -56,7 +56,7 @@ const Calendar = ({ value = new Date(), onChangeDate }) => {
       setSelectedDate(date.d);
 
       const fomatedDate = `${moment(selectedMonth).format('YYYY-MM').toString()}-${date.d}`;
-      onChangeDate(moment(fomatedDate).format('YYYY-MM-DD').toString());
+      onChangeDate(moment(fomatedDate, 'YYYY-MM-D').format('YYYY-MM-DD').toString());
     }
   };
 
@@ -99,7 +99,7 @@ const Calendar = ({ value = new Date(), onChangeDate }) => {
         <i className="iconfont icon-left" onClick={handleSelectPreMonth}></i>
         <span>{moment(selectedMonth).format('YYYY-MM')}</span>
         <i
-          className={cname('iconfont', 'icon-right', { 'unclickable': moment().diff(selectedMonth, 'M') === 0 })}
+          className={cname('iconfont', 'icon-right', { 'unclickable': moment().diff(selectedMonth, 'M') <= 0 })}
           onClick={handleSelectNextMonth}
         ></i>
       </div>
@@ -116,7 +116,11 @@ const Calendar = ({ value = new Date(), onChangeDate }) => {
 
       <div className="date">
         {monthDates.map((date, index) => (
-          <span className={cname('cell', { 'selected': date.d === selectedDate, 'unclickable': date.d && !date.clickable })} key={index} onClick={() => { handleSelectedDate(date); }}>{date.d}</span>
+          <span
+            className={cname('cell', { 'selected': date.d === selectedDate, 'unclickable': date.d && !date.clickable })}
+            key={index}
+            onClick={() => { handleSelectedDate(date); }}
+          >{date.d}</span>
         ))}
       </div>
 
